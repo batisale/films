44b946038b5ce8085204352300834b189b471672
InFeRnOJeReZ <InFeRnOJeReZ@192.168.1.11>
2017-03-30 15:31:14 +0200

    FilmDAO

---
+++ src/main/java/com/dbg/dao/film/FilmDao.java
@@ -0,0 +1,14 @@
+package com.dbg.dao.film;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dbg.model.film.Film;
+@Repository
+public interface FilmDao extends PagingAndSortingRepository<Film, Integer> {
+
+	public List<Film> findByNameContaining(String title);
+
+}
